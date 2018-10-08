5c7cdb6bcd8f5807f8d8d9cc8567968ba8c78914
sydneycode <sstaggs12@gmail.com>
2018-10-08 00:19:18 -0700

    added seeds file to populate the initial petspacedb database

--- models/seeds.sql
+++ models/seeds.sql
@@ -1,2 +1,100 @@
 INSERT INTO borrowers (email, password, name, streetAddress, city, state, zip)
-VALUES ('bob@gmail.com', 'cats', 'Bob McDonald', '23892 Kite Hill Dr.', 'Laguna Niguel', 'CA', '92677');
+VALUES ('bob@gmail.com', 'cats', 'Bob McDonald', '23892 Kite Hill Dr', 'Laguna Niguel', 'CA', '92677');
+INSERT INTO borrowers (email, password, name, streetAddress, city, state, zip)
+VALUES ('chrisrice@gmail.com', 'password2', 'Chris Rice', '84651 Wishbone St.', 'Los Angeles', 'CA', '92888');
+INSERT INTO borrowers (email, password, name, streetAddress, city, state, zip)
+VALUES ('jeffjefferson@gmail.com', 'password3', 'Jeff Sessions', '23892 Kite Hill Dr.', 'Orange', 'CA', '91578');
+INSERT INTO borrowers (email, password, name, streetAddress, city, state, zip)
+VALUES ('dandaneilson@gmail.com', 'password4', 'Dan Danielson', '23892 Kite Hill Dr.', 'Garden Grove', 'CA', '92622');
+INSERT INTO borrowers (email, password, name, streetAddress, city, state, zip)
+VALUES ('taybae@gmail.com', 'password1', 'Tay Bae', '49514 Golden Hoops Ln.', 'Irvine', 'CA', '92489');
+INSERT INTO borrowers (email, password, name, streetAddress, city, state, zip)
+VALUES ('yuliana@gmail.com', 'dog', 'Yuliana Castellanos', '325 West Adams st.', 'Los Angeles', 'CA', '90007');
+INSERT INTO borrowers (email, password, name, streetAddress, city, state, zip)
+VALUES ('jewels@gmail.com', 'dog', 'Julie Mendoza', '616 St Paul Ave.', 'Los Angeles', 'CA', '90017');
+INSERT INTO borrowers (email, password, name, streetAddress, city, state, zip)
+VALUES ('doggy@gmail.com', 'dog', 'Jewels Jewel', '589 St Paul Ave.', 'Los Angeles', 'CA', '90017');
+INSERT INTO borrowers (email, password, name, streetAddress, city, state, zip)
+VALUES ('catluvtaylor@gmail.com', 'dog', 'Chris Taylor  ', '589 St Paul Ave.', 'Los Angeles', 'CA', '90017');
+INSERT INTO borrowers (email, password, name, streetAddress, city, state, zip)
+VALUES ('bella@gmail.com', 'dog', 'Issa Belly  ', ' 14232 Dyer St.', 'Sylmar', 'CA', '91342');
+INSERT INTO borrowers (email, password, name, streetAddress, city, state, zip)
+VALUES ('mia@gmail.com', 'dog', 'Mia Bella  ', ' 560 Minihaha St.', 'Mission hills', 'CA', '91345');
+INSERT INTO borrowers (email, password, name, streetAddress, city, state, zip)
+VALUES ('bambam@gmail.com', 'dog', 'Sam Bam  ', ' 4060 Mission  Drive.', 'Mission hills', 'CA', '91345');
+INSERT INTO borrowers (email, password, name, streetAddress, city, state, zip)
+VALUES ('lidia@gmail.com', 'dog', 'Lidia Smith  ', ' 550 N Figueroa St', 'Los Angeles', 'CA', '90012');
+INSERT INTO borrowers (email, password, name, streetAddress, city, state, zip)
+VALUES ('alison@gmail.com', 'password$', 'Alison Smith', '25 Monroe Rd', 'Monrovia', 'CA', '91016');
+INSERT INTO borrowers (email, password, name, streetAddress, city, state, zip)
+VALUES ('carys@gmail.com', 'p1a2s3s4', 'Carys Douglas', '21 Serenity Ln', 'Laguna Niguel', 'CA', '92677');
+INSERT INTO borrowers (email, password, name, streetAddress, city, state, zip)
+VALUES ('alexis@gmail.com', 'alex', 'Alexis Castle', '307 Finesse', 'San Juan Capistrano', 'CA', '92675');
+INSERT INTO borrowers (email, password, name, streetAddress, city, state, zip)
+VALUES ('gary@gmail.com', 'snail', 'Gary Johnston', '46 Wyland St', 'Dana Point', 'CA', '92624');
+
+
+INSERT INTO providers (email, password, name, streetAddress, city, state, zip, category)
+VALUES ('allens@animalshelter.org', 'allensspecial', "Allen's Building of Various Animals", '123 Example St.', 'Los Angeles', 'CA', '92672', 'Shelter');
+INSERT INTO providers (email, password, name, streetAddress, city, state, zip, category)
+VALUES ('strangebbqsmell@animalshelter.org', 'password2', 'BBQ(Bosnea Basin Quartile', '321 Street St.', 'Riverside', 'CA', '12345', 'Shelter');
+INSERT INTO providers (email, password, name, streetAddress, city, state, zip, category)
+VALUES ('largefootsteps@animalshelter.org', 'welovepets', 'Large Footsteps', '800 Brookforest Drive', 'Shorewood', 'IL', '60404', 'Shelter');
+INSERT INTO providers (email, password, name, streetAddress, city, state, zip, category)
+VALUES ('animalhuggers@animalshelter.org', 'animalhuggers', 'We Hug Animals Only', '846 Daniel St.', 'Irvine', 'CA', '92672', 'Shelter');
+INSERT INTO providers (email, password, name, streetAddress, city, state, zip, category)
+VALUES ('westlashelter@shelter.org', 'welovepets', 'West Los Angeles Animal Shelter', '11361 Pico Blvd', 'Los Angeles', 'CA', '90064', 'Shelter');
+INSERT INTO providers (email, password, name, streetAddress, city, state, zip, category)
+VALUES ('southla@shelter.org', 'welovepets', 'South LA Animal Shelter', '1850 W 60th St.', 'Los Angeles', 'CA', '90047', 'Shelter');
+INSERT INTO providers (email, password, name, streetAddress, city, state, zip, category)
+VALUES ('northcentral@animalshelter.org', 'welovepets', 'North Central Animal Shelter', '3201 Lacy St', 'Los Angeles', 'CA', '90031', 'Shelter');
+INSERT INTO providers (email, password, name, streetAddress, city, state, zip, category)
+VALUES ('westla@animalshelter.org', 'welovepets', 'West Los Angeles Animal Shelter', '11361 Pico Blvd', 'Los Angeles', 'CA', '90064', 'Shelter');
+INSERT INTO providers (email, password, name, streetAddress, city, state, zip, category)
+VALUES ('eastvalley@animalshelter.org', 'welovepets', 'City East Valley Animal Shelter', '14409 Vanowen St', 'Van Nuys', 'CA', '91405', 'Shelter');
+INSERT INTO providers (email, password, name, streetAddress, city, state, zip, category)
+VALUES ('harboranimal@animalshelter.org', 'welovepets', 'Harbor Animal Care Center', '957 N Gaffey St', 'San Pedro', 'CA', '90731', 'Shelter');
+INSERT INTO providers (email, password, name, streetAddress, city, state, zip, category)
+VALUES ('sanclementeshelter@animalshelter.org', 'welovepets', 'San Clemente Animal Shelter', '221 Avenida Fabricante', 'San Clemente', 'CA', '92672', 'Shelter');
+INSERT INTO providers (email, password, name, streetAddress, city, state, zip, category)
+VALUES ('newportbeachshelter@animalshelter.org', 'password900', 'Newport Beach Animal Shelter', '20302 Riverside Dr', 'Newport Beach', 'CA', '92660', 'Shelter');
+INSERT INTO providers (email, password, name, streetAddress, city, state, zip, category)
+VALUES ('tara@gmail.org', 'taralovescats', 'Tara Ferguson', '34 Forrest Ln', 'San Clemente', 'CA', '92672', 'Owner');
+INSERT INTO providers (email, password, name, streetAddress, city, state, zip, category)
+VALUES ('ashley@gmail.org', 'password200', 'Ashley Donner', '395 Star St', 'Dana Point', 'CA', '92624', 'Owner');
+INSERT INTO providers (email, password, name, streetAddress, city, state, zip, category)
+VALUES ('ronald@gmail.org', 'ronron', 'Ron Weber', '22 Alasdair St', 'San Clemente', 'CA', '92672', 'Owner');
+
+
+INSERT INTO pets (name, category, breed, gender, temperament, photoURL, provider_id)
+VALUES ('Maymoon', 'Cat', 'Tabby', 'Female', 2, 'https://i.imgur.com/l0YAN0c.jpg',1);
+INSERT INTO pets (name, category, breed, gender, temperament, photoURL, provider_id)
+VALUES ('Spots', 'Cat', 'Calico', 'Female', 4, 'https://www.catster.com/wp-content/uploads/2018/03/Calico-cat-curled-up-asleep.jpg', 1);
+INSERT INTO pets (name, category, breed, gender, temperament, photoURL, provider_id)
+VALUES ('Doggo', 'Cat', 'Tabby', 'Female', 2, 'https://i.pinimg.com/originals/19/2e/3d/192e3d905f9e6a320b3349b0ade9a950.jpg',4);
+INSERT INTO pets (name, category, breed, gender, temperament, photoURL, provider_id)
+VALUES ('Daniel', 'Dog', 'Husky', 'Male', 8, 'https://pbs.twimg.com/profile_images/639599645925076994/7Egv8qXQ.jpg',1);
+INSERT INTO pets (name, category, breed, gender, temperament, photoURL, provider_id)
+VALUES ('Chris', 'Cat', 'Domestic Short Hair', 'Male', 4, 'https://simonscat.com/wp-content/uploads/2017/10/2593-1-344364__xwTimjZh-768x1024.jpg',1);
+INSERT INTO pets (name, category, breed, gender, temperament, photoURL, provider_id)
+VALUES ('Taylor', 'Cat', 'Maine Coon', 'Female', 2, 'https://s3.amazonaws.com/imagesroot.rescuegroups.org/webpages/s581n8d6purvfgt.jpeg',1);
+INSERT INTO pets (name, category, breed, gender, temperament, photoURL, provider_id)
+VALUES ('Chiqui', 'Cat', 'Tabby', 'Female', 1, 'https://s3.amazonaws.com/filestore.rescuegroups.org/8835/pictures/animals/13637/13637162/61114445_500x500.jpg', 5);
+INSERT INTO pets (name, category, breed, gender, temperament, photoURL, provider_id)
+VALUES ('Milo', 'Cat', 'Tabby', 'Male', 2, 'https://s3.amazonaws.com/filestore.rescuegroups.org/8835/pictures/animals/13637/13637163/61114447_500x500.jpg', 1);
+INSERT INTO pets (name, category, breed, gender, temperament, photoURL, provider_id)
+VALUES ('Comet', 'Cat', 'Calico', 'Female', 1, 'https://s3.amazonaws.com/filestore.rescuegroups.org/8835/pictures/animals/12962/12962939/57280912_500x500.jpg', 7);
+INSERT INTO pets (name, category, breed, gender, temperament, photoURL, provider_id)
+VALUES ('Lala Land', 'Dog', 'Pitbull', 'Female', 2, 'https://s3.amazonaws.com/filestore.rescuegroups.org/8835/pictures/animals/13559/13559356/60596188_500x500.jpg', 1);
+INSERT INTO pets (name, category, breed, gender, temperament, photoURL, provider_id)
+VALUES ('Benjamin Button', 'Dog', 'Pitbull', 'Male', 3,'https://s3.amazonaws.com/filestore.rescuegroups.org/8835/pictures/animals/13559/13559353/60596184_500x500.jpg', 12);
+INSERT INTO pets (name, category, breed, gender, temperament, photoURL, provider_id)
+VALUES ('Claire', 'Dog', 'Chihuahua', 'Female', 2, 'https://s3.amazonaws.com/filestore.rescuegroups.org/8835/pictures/animals/13341/13341562/59075380_500x500.jpg', 1);
+INSERT INTO pets (name, category, breed, gender, temperament, photoURL, provider_id)
+VALUES ('Tiffany', 'Dog', 'Golden Retriever', 'Female', 2, 'https://vetstreet.brightspotcdn.com/dims4/default/d742db0/2147483647/thumbnail/645x380/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2F98%2Fd98250a0d311e0a2380050568d634f%2Ffile%2FGolden-Retriever-3-645mk062411.jpg', 5);
+INSERT INTO pets (name, category, breed, gender, temperament, photoURL, provider_id)
+VALUES ('Caramel', 'Cat', 'Orange Tabby', 'Male', 8, 'https://www.thepurringtonpost.com/wp-content/uploads/2017/06/cat-2333413_960_720.jpg', 2);
+INSERT INTO pets (name, category, breed, gender, temperament, photoURL, provider_id)
+VALUES ('Frosty', 'Dog', 'Siberian Husky', 'Male', 4, 'https://g77v3827gg2notadhhw9pew7-wpengine.netdna-ssl.com/wp-content/uploads/2017/11/husky-2699136_1920-1-1024x678.jpg', 15);
+INSERT INTO pets (name, category, breed, gender, temperament, photoURL, provider_id)
+VALUES ('Cocoa', 'Cat', 'Tonkinese', 'Female', 7, 'http://dinoanimals.com/wp-content/uploads/2016/07/Tonkinese_cat-_5.jpg', 15);
